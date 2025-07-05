dd9980b4ee431aaf3fc0b8141c81ad9d52896fed
Tô Phạm Thành Đạt <todat2207@gmail.com>
2025-07-05 21:06:22 +0700

    add debug vs bai 16, 17 5/7/25

---
+++ bai_16_if_else/main.js
@@ -0,0 +1,10 @@
+//16: if else
+
+let dtb = parseFloat(prompt("Nhập điểm trung bình: "));
+// kiểm tra đk
+if (dtb >= 5.0) {
+  console.log("Đậu");
+} else {
+  console.log("Rớt");
+}
+// nếu điểm trung bình >= 5.0 thì in ra "Đậu", ngược lại in ra "Rớt"
---
+++ bai_17_toan_tu_3_ngoi/main.js
@@ -0,0 +1,8 @@
+//17: toán tử 3 ngôi
+let so = 10;
+let mes = so > 0 ? "Số dương" : "Số âm";
+console.log(mes); // in ra "Số dương" nếu so > 0, ngược lại in ra "Số âm"
+
+let so1 = 20;
+let mes1 = so1 % 2 == 0 ? "số chẵn" : "số lẻ";
+console.log(mes1); // in ra "số chẵn" nếu so1 là
